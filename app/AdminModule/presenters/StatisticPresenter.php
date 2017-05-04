ace6844f86c11b25d93f9809638f1d0ea884c85e
PetrBecka <petrbecka42@gmail.com>
2017-05-04 20:44:23 +0200

    cena v bunkach

--- app/AdminModule/presenters/StatisticPresenter.php
+++ app/AdminModule/presenters/StatisticPresenter.php
@@ -23,8 +23,9 @@ class StatisticPresenter extends BasePresenter
     {
         if(isset($this->project)){
             $this->template->anyVariable = 'any value';
-            $arrJSON = [];
             $project = $this->projectFacade->getProject($this->project);
+            $arrJSON = [];
+
             foreach ($project->phases as $phase){
                 $phaseJSON= [];
                 foreach($phase->risks as $risk){
@@ -41,12 +42,33 @@ class StatisticPresenter extends BasePresenter
                 array_push($arrJSON, $phaseJSON);
             }
 
+            $this->template->allProjects = [];
             $this->template->JSONData = json_encode($arrJSON,JSON_UNESCAPED_UNICODE);
         } else {
 	        $projects = $this->projectFacade->getProjects();
+            $arrJSON = [];
+
+            foreach ($projects as $project){
+                foreach ($project->phases as $phase){
+                    $phaseJSON= [];
+                    foreach($phase->risks as $risk){
+                        $riskJSON = [];
+                        array_push($riskJSON, $risk->id);
+                        array_push($riskJSON, $risk->name);
+                        array_push($riskJSON, $risk->probability);
+                        array_push($riskJSON, $risk->money);
+                        array_push($riskJSON, $risk->severity);
+                        array_push($riskJSON, $risk->primaryCause);
+                        array_push($riskJSON, $risk->result);
+                        array_push($phaseJSON, $riskJSON);
+                    }
+                    array_push($arrJSON, $phaseJSON);
+                }
+            }
 
 	        //@TODO projet všechny projekty a pro všechny udělat to co máš vejš a hodit to do jedné tabulky
-	        $this->template->JSONData = json_encode([],JSON_UNESCAPED_UNICODE);
+            $this->template->allProjects = $projects;
+	        $this->template->JSONData = json_encode($arrJSON, JSON_UNESCAPED_UNICODE);
         }
     }
 
